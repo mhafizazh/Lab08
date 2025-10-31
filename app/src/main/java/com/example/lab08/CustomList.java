ba0743944ab1c7a0b2ed99729ad7594794651f40
Muhammad Hafiz Azhari <mhafiz.azh@gmail.com>
2025-10-30 23:40:15 -0600

    delete city pass

--- app/src/main/java/com/example/lab08/CustomList.java
+++ app/src/main/java/com/example/lab08/CustomList.java
@@ -17,6 +17,15 @@ public class CustomList {
         return cities.contains(city);
     }
 
+    public void deleteCity(City city) {
+        if (hasCity(city)) {
+            cities.remove(city);
+        } else {
+            throw new IllegalArgumentException("City not found");
+        }
+    }
+
+
 
 
 
